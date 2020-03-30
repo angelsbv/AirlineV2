ec8b8ceb65456676e49024cce714e288f97baf16
k0o <sb.knot@gmail.com>
2020-03-29 21:37:49 -0400

    v2.0

---
+++ AirlineV2/AirlineV2/Controllers/FlightController.cs
@@ -0,0 +1,130 @@
+﻿using AirlineV2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airline.Controllers
+{
+    public class FlightController : Controller
+    {
+        private readonly AirlineV2Context ctx;
+        public FlightController(AirlineV2Context context)
+        {
+            ctx = context;
+        }
+        [Route("vuelos"), Route("vuelos/index")]
+        public async Task<IActionResult> Index()
+        {
+            var model = ctx.Flights.Include(f => f.aircraft);
+            return View(await model.ToListAsync());
+        }
+
+        [Route("vuelos/agregar")]
+        [HttpGet]
+        public IActionResult AddFlg()
+        {
+            ViewData["AcID"] = new SelectList(ctx.Aircrafts, "AcID", "AcModel");
+            return View();
+        }
+
+        [Route("vuelos/agregar")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddFlg(Flight vmFr)
+        {
+            if (ModelState.IsValid)
+            {
+                ctx.Flights.Add(vmFr);
+                await ctx.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AcID"] = new SelectList(ctx.Aircrafts, "AcID", "AcModel", vmFr.AcID);
+            return View(vmFr);
+        }
+
+        [Route("vuelos/editar/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> EditFlg(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var flight = await ctx.Flights.FindAsync(id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+            ViewData["AcID"] = new SelectList(ctx.Aircrafts, "AcID", "AcModel", flight.AcID);
+            return View(flight);
+        }
+
+        [Route("vuelos/editar/{id}")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditFlg(int id, [Bind("FlgID,AcID,FlgDeparture,FlgArrival,FlgFare,FlgCategory")] Flight flight)
+        {
+            if (id != flight.FlgID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    ctx.Update(flight);
+                    await ctx.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ctx.Flights.Any(e => e.FlgID == flight.FlgID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AcID"] = new SelectList(ctx.Aircrafts, "AcID", "AcModel", flight.AcID);
+            return View(flight);
+        }
+
+        [Route("vuelos/eliminar/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> DelFlg(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var flight = await ctx.Flights
+                .Include(f => f.aircraft)
+                .FirstOrDefaultAsync(m => m.FlgID == id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+
+            return View(flight);
+        }
+
+        [Route("vuelos/eliminar/{id}")]
+        [HttpPost, ActionName("DelFlg")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SBCDelFlg(int id)
+        {
+            var flight = await ctx.Flights.FindAsync(id);
+            ctx.Flights.Remove(flight);
+            await ctx.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ AirlineV2/AirlineV2/Models/AirlineV2Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AirlineV2.Models
+{
+    public class AirlineV2Context : DbContext
+    {
+        public AirlineV2Context(DbContextOptions<AirlineV2Context> options) : base(options) { }
+
+        public DbSet<Aircraft> Aircrafts { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Flight> Flights { get; set; }
+
+        public DbSet<FlightAssignedTo> FlightAssignedEmps { get; set; }
+
+        public DbSet<Client> Clients { get; set; }
+    }
+}
